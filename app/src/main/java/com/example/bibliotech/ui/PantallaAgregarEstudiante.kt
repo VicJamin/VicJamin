d26abba6c34d4fa138058455852767119c6ef834
Jamin Martinez <jaminvictor5@gmail.com>
2026-09-24 11:16:05 -0600

    Agregamos la pantalla.  PantallaAgregarEstudiante.kt

--- app/src/main/java/com/example/bibliotech/ui/PantallaAgregarEstudiante.kt
+++ app/src/main/java/com/example/bibliotech/ui/PantallaAgregarEstudiante.kt
@@ -1,6 +1,8 @@
 package com.example.bibliotech.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,6 +18,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bibliotech.viewmodel.EstudianteViewModel
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.unit.dp
+
 @OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
@@ -77,6 +83,10 @@ fun PantallaAgregarEstudiante(
 
         Column(
             modifier = Modifier.padding(paddingValues)
+                .fillMaxSize()
+                .background(Color.Black)
+                .verticalScroll(rememberScrollState())
+                .padding(20.dp)
         ){
 
 
