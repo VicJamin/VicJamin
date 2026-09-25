6df3a7f107a87153605413e7e786ddb30e390891
Jamin Martinez <jaminvictor5@gmail.com>
2026-09-25 09:55:21 -0600

    Terminamos la PantallaAgregarEstudiante.kt fecha(25/9/2026) hora: 9:56am

--- app/src/main/java/com/example/bibliotech/ui/PantallaAgregarEstudiante.kt
+++ app/src/main/java/com/example/bibliotech/ui/PantallaAgregarEstudiante.kt
@@ -2,7 +2,10 @@ package com.example.bibliotech.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,7 +23,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.ui.unit.dp
+import com.example.bibliotech.model.Estudiante
 
 @OptIn(ExperimentalMaterial3Api::class)
 
@@ -88,6 +99,207 @@ fun PantallaAgregarEstudiante(
                 .verticalScroll(rememberScrollState())
                 .padding(20.dp)
         ){
+            //Carnet del estudiante
+            OutlinedTextField(
+                value = carnet,
+                onValueChange = { carnet = it },
+                label = { Text("Número de Carnet") },
+                modifier = Modifier.fillMaxSize(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White,
+                    focusedBorderColor = Color(0xFF3B82F6),
+                    unfocusedBorderColor = Color.White,
+                    focusedLabelColor = Color(0xFF60A5FA),
+                    unfocusedLabelColor = Color.LightGray
+                )
+
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            //nombres del estudiante
+            OutlinedTextField(
+                value = nombres,
+                onValueChange = { nombres = it },
+                label = { Text("Nombres del estudiante") },
+                modifier = Modifier.fillMaxSize(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White,
+                    focusedBorderColor = Color(0xFF3B82F6),
+                    unfocusedBorderColor = Color.White,
+                    focusedLabelColor = Color(0xFF60A5FA),
+                    unfocusedLabelColor = Color.LightGray
+                )
+
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+
+            //Apellidos del estudiante
+            OutlinedTextField(
+                value = apellidos,
+                onValueChange = { apellidos = it },
+                label = { Text("Apellidos del estudiante") },
+                modifier = Modifier.fillMaxSize(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White,
+                    focusedBorderColor = Color(0xFF3B82F6),
+                    unfocusedBorderColor = Color.White,
+                    focusedLabelColor = Color(0xFF60A5FA),
+                    unfocusedLabelColor = Color.LightGray
+                )
+
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+
+            //Grado del estudiante
+            ExposedDropdownMenuBox(
+                expanded = expandirGrado,
+                onExpandedChange = { expandirGrado = !expandirGrado }
+            )
+             {
+                OutlinedTextField(
+                    value = grado,
+                    onValueChange = {},
+                    label = { Text("Grado") },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expandirGrado
+                        )
+
+                    },
+                    modifier = Modifier.fillMaxSize(),
+
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        focusedBorderColor = Color(0xFF3B82F6),
+                        unfocusedBorderColor = Color.White,
+                        focusedLabelColor = Color(0xFF60A5FA),
+                        unfocusedLabelColor = Color.LightGray
+                    )
+                )
+
+                 ExposedDropdownMenu(
+                     expanded = expandirGrado,
+                     onDismissRequest = { expandirGrado = false }
+                 ) {
+                     grados.forEach {
+                         DropdownMenuItem(
+                             text = { Text(it) },
+                             onClick = {
+                                 grado = it
+                                 expandirGrado = false
+                             }
+
+                         )
+                     }
+                 }
+
+
+             }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            //Seccion del estudiante
+            ExposedDropdownMenuBox(
+                expanded = expandirGrado,
+                onExpandedChange = { expandirGrado = !expandirGrado }
+            )
+            {
+                OutlinedTextField(
+                    value = seccion,
+                    onValueChange = {},
+                    label = { Text("Seccion") },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expandirSeccion
+                        )
+
+                    },
+                    modifier = Modifier.fillMaxSize(),
+
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        focusedBorderColor = Color(0xFF3B82F6),
+                        unfocusedBorderColor = Color.White,
+                        focusedLabelColor = Color(0xFF60A5FA),
+                        unfocusedLabelColor = Color.LightGray
+                    )
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expandirSeccion,
+                    onDismissRequest = { expandirSeccion = false }
+                ) {
+                    secciones.forEach {
+                        DropdownMenuItem(
+                            text = { Text(it) },
+                            onClick = {
+                                seccion = it
+                                expandirSeccion = false
+                            }
+
+                        )
+                    }
+                }
+
+
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            //Estado del estudiante
+            Row{
+                Checkbox(
+                    checked = activo,
+                    onCheckedChange = {activo = it}
+                )
+
+                Text(
+                    text = "Activo",
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            //Botones
+
+            Button(onClick = {
+                val nuevoEstudiante = Estudiante(
+                    carnet = carnet,
+                    nombres = nombres,
+                    apellidos = apellidos,
+                    grado = grado,
+                    seccion = seccion,
+                    activo = activo
+                )
+                viewModel.insertarEstudiante(nuevoEstudiante)
+                onGuardar()
+            },
+                modifier = Modifier.fillMaxSize()
+            ){
+                Text("Guardar Estudiante")
+
+            }
+
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Button(onClick = {
+                onCancelar()
+            },
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text("Cancelar")
+
+            }
 
 
             }
